bc8ff1d287ca13a40e1cec928f7a4cbfe0ef1ac5
Yansell <yansellrivasdiaz@gmail.com>
2018-11-11 23:25:15 -0400

    Se agrego funcionalidades a las funciones del evento submit formulario login y registrase

--- styles/js/functions.js
+++ styles/js/functions.js
@@ -6,7 +6,12 @@ window.addEventListener('load',function(){
             e.preventDefault();
             var username = document.getElementById('username').value;
             var password = document.getElementById('password').value;
-            alert(username + '-' + password);
+            if(username != '' && password != ''){
+                alert(username + '-' + password);
+            }else{
+                alert('Campos obligatorios');
+                document.getElementById('username').focus();
+            }
         },false);
     }
     // Evento click del formulario registrarse
@@ -18,7 +23,17 @@ window.addEventListener('load',function(){
             var email = document.getElementById('email').value;
             var password = document.getElementById('password').value;
             var re_password = document.getElementById('re-password').value;
-            alert(name + '-' + email);
+            if(name != '' && email != '' && password != '' && re_password != ''){
+                if(password === re_password){
+                    alert('datos validados');
+                }else{
+                    alert('Contraseñas no coinciden');
+                    document.getElementById('re-password').focus();
+                }
+            }else{
+                alert('Todos los campos con requeridos');                
+                document.getElementById('name').focus();
+            }
         },false);
     }
 });
